59eedc60266636d15d5f5c60e17b69c4fab0099e
ubuntu <prints.ann@gmail.com>
2024-11-12 11:02:05 +0000

    Release 2024.3

---
+++ blade-core/src/main/java/com/hellokaton/blade/Generate.java
@@ -0,0 +1,16 @@
+package com.hellokaton.blade;
+
+public class Generate {
+    int xxx = 5;
+
+
+    public Generate() {
+        this.xxx = xxx;
+    }
+
+    public static void main(String[] args) {
+        Generate myObj = new Generate();
+
+
+    }
+}
